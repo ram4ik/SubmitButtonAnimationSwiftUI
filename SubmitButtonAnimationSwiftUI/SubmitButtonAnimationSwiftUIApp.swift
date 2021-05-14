019e097ee6414a3d2f35dc02e9f9427939f18a06
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-05-14 15:06:51 +0300

    Initial Commit

---
+++ SubmitButtonAnimationSwiftUI/SubmitButtonAnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubmitButtonAnimationSwiftUIApp.swift
+//  SubmitButtonAnimationSwiftUI
+//
+//  Created by Ramill Ibragimov on 14.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct SubmitButtonAnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
